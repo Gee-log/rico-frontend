3b5776432f9d76a8caf42aa74c8774b7b22e9364
Xen VM <xen@debian.vm>
2017-06-14 14:58:15 +0700

    Fix icon

--- UI/mysite/mysite/settings.py
+++ UI/mysite/mysite/settings.py
@@ -126,6 +126,24 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/error.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
