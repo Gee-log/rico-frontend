a396d0544ab46486c7187c4d5f708bf05a3ac692
XENO\suraneti <suraneti.rod@gmail.com>
2017-06-16 14:56:31 +0700

    Current Alarm / Alarm History : UTC date Add Conn History : Search filter

--- UI/mysite/personal/static/personal/js/alarm.js
+++ UI/mysite/personal/static/personal/js/alarm.js
@@ -16,6 +16,7 @@ var patterns = [
 ];
 
 function updateSaveUrl(time) {
+
   console.log('updateSaveUrl', time);
   $("#save").prop("href", "/2/" + time);
 }
@@ -94,6 +95,7 @@ function randomAlert() {
         },
         success: function (e) {
             console.log('randomAlert success', e);
+            createTable(e);
 
         }
     });
--- UI/mysite/personal/static/personal/js/connection.js
+++ UI/mysite/personal/static/personal/js/connection.js
@@ -1,18 +1,38 @@
-$(document).ready(function() {
-  $.ajax({
-      type: 'GET',
-      url: '/connections/',
-      data: {
-        act: "connected",
-        act: "disconnected"
-      },
-      success: function(data) {
-          connected_port = data;
-
-          // do something
-          for (i in connected_port) {
-          console.log(connected_port[i]);
-          }
-      }
-  });
+$(document).ready(function(e) {
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+        e.preventDefault();
+        var param = $(this).attr("href").replace("#", "");
+        var concept = $(this).text();
+        $('.search-panel span#search_concept').text(concept);
+        $('.input-group #search_param').val(param);
+    });
 });
+
+    $('#uItem li').click(function() {
+        value1 = $(this).attr('value');
+        console.log(value1);
+    });
+
+    function myFunction() {
+        var input;
+        var filter;
+        var table;
+        var tr;
+        var td;
+        var i;
+
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTableBody");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[value1];
+            if (td) {
+                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
