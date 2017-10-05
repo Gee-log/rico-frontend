c7449ba3a3d6a93be45632c28e23af9b2867bc3d
LAPTOP-Suraneti <suraneti.rod@gmail.com>
2017-10-05 16:14:34 +0700

    Update .gitignore

--- backend/webapp/tests.py
+++ backend/webapp/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+import json
+from django.urls import reverse
+from django.test import TestCase, Client
+from webapp.models import Port
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+# initialize the APIClient app
+client = Client()
+
+
+class CreateConnectiontest(TestCase):
+    """ Test module for inserting a new puppy """
+
+    def test_create_eport(self):
+
+        for i in range(1, 145, 1):
+            data = {'direction': 'E', 'number': i}
+            response = client.post('/ports/', data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            print(data, response.status_code)
+
+    def test_create_wport(self):
+
+        for i in range(1, 145, 1):
+            data = {'direction': 'W', 'number': i}
+            response = client.post('/ports/', data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            print(data, response.status_code)
+
+    def test_create_connection(self):
+
+        for i in range(1, 145, 1):
+            data = {'east': i, 'west': i, 'action': 'connect'}
+            response = client.post('/connections/', data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            print(data, response.status_code)
