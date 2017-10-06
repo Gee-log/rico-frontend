4cc816c785649b3b2a8a0c49ef1d4a647715d71b
Berm MBA11 <berm@mba11.mac>
2017-10-06 11:14:44 +0700

    ./manage.py -> python manage.py

--- backend/start.sh
+++ backend/start.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
-tables=`./manage.py showmigrations`
+tables=`python manage.py showmigrations`
 if [[ $tables == *"webapp"* ]] ; then
   echo RICO: no need to migrate
 else
   echo RICO: migrate.py makemigrations --empty webapp
-  ./manage.py makemigrations --empty webapp
-  ./manage.py makemigrations
-  ./manage.py migrate
-  ./manage.py collectstatic --no-input
+  python manage.py makemigrations --empty webapp
+  python manage.py makemigrations
+  python manage.py migrate
+  python manage.py collectstatic --no-input
 fi
 
-ports=`./manage.py dumpdata webapp.port`; chrlen=${#ports} # 32320
+ports=`python manage.py dumpdata webapp.port`; chrlen=${#ports} # 32320
 echo ports: $chrlen
 if [ $chrlen -gt 20000 ]; then
   echo RICO: no need to loaddata
 else
   echo RICO: migrate.py loaddata
-  ./manage.py loaddata data/Wport_data.json
-  ./manage.py loaddata data/Eport_data.json
+  python manage.py loaddata data/Wport_data.json
+  python manage.py loaddata data/Eport_data.json
 fi
 
 /usr/local/bin/gunicorn backend.wsgi:application -w 2 -b :8000
