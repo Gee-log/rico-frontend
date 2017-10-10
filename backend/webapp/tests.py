8576b046f94b7962f0a8d6c1827cc87b9b975cdb
LAPTOP-Suraneti <suraneti.rod@gmail.com>
2017-10-10 14:08:30 +0700

    Update url in tests.py

--- backend/webapp/tests.py
+++ backend/webapp/tests.py
@@ -36,71 +36,61 @@ class TestWhitewalker(APITestCase):
         self.assertEqual(out['status'], 'success')
 
     def test_no_east_input_connect(self):
-        url = 'http://localhost:8000/connections/'
         data = {'west': west, 'action': 'connect'}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, 'No east')
     
     def test_no_west_input_connect(self):
-        url = 'http://localhost:8000/connections/'
         data = {'east': east, 'action': 'connect'}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, 'No west')
 
     def test_correct_input_disconnect(self):
-        url = 'http://localhost:8000/connections/'
         data = {'east': east, 'west': west, 'action': 'disconnect'}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, data)
 
     def test_no_east_input_disconnection(self):
-        url = 'http://localhost:8000/connections/'
         data = {'west': west, 'action': 'disconnect'}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, 'No east')
 
     def test_no_west_input_disconnection(self):
-        url = 'http://localhost:8000/connections/'
         data = {'east': east, 'action': 'disconnect'}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, 'No west')
 
     def test_correct_input_debug(self):
-        url = 'http://localhost:8000/connections/'
         data = {'east': east, 'west': west, 'action': 'debug'}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, data)
 
     def test_no_east_input_debug(self):
-        url = 'http://localhost:8000/connections/'
         data = {'west': west, 'action': 'debug'}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, 'No east')
 
     def test_no_west_input_debug(self):
-        url = 'http://localhost:8000/connections/'
         data = {'east': east, 'action': 'debug'}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, 'No west')
 
     def test_no_action_input_connect(self):
-        url = 'http://localhost:8000/connections/'
         data = {'east': east, 'west': west}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, 'No action')
 
     def test_no_input_connection(self):
-        url = 'http://localhost:8000/connections/'
         data = {}
-        resp = self.client.post(url, data, format='json')
+        resp = self.client.post('{}/connections/'.format(self.HOST), data, format='json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, 'No action')
